b3158195b968e1f3cd4cf88310772fe8541e3712
Richard Bowker <richardbowker@monkeygroover.com>
2019-02-03 00:23:48 +0000

    update 2.1

--- day_2/part2-1.exs
+++ day_2/part2-1.exs
@@ -2,21 +2,16 @@
 
 defmodule Day2 do
     def letter_frequencies(id) do
-        String.graphemes(id)
-        |> Enum.reduce(
+        Enum.reduce(
+            String.graphemes(id),
             %{},
             fn (char, acc) ->
                 Map.update(acc, char, 1, fn(count) -> count + 1 end)
             end)
     end
 
-    def count_pairs_and_triples(freq_map) do
-        Enum.reduce(
-            freq_map,
-            {0,0},
-            fn (freq_map, {pairs, triples}) ->
-                {pairs + contains_freq(freq_map, 2), triples + contains_freq(freq_map, 3)}
-            end)
+    def pairs_and_triples_reducer(freq_map, {pairs, triples}) do
+        {pairs + contains_freq(freq_map, 2), triples + contains_freq(freq_map, 3)}
     end
 
     defp contains_freq(freq_map, freq) do
@@ -26,12 +21,11 @@ defmodule Day2 do
         end
     end
 
-    def checksum({p, t}) do p * t end
 end
 
 IO.stream(:stdio, :line)
 |> Stream.map(&String.trim/1)
 |> Stream.map(&Day2.letter_frequencies/1)
-|> Day2.count_pairs_and_triples
-|> Day2.checksum
+|> Enum.reduce({0,0}, &Day2.pairs_and_triples_reducer/2)
+|> (fn {p,t} -> p * t end).()
 |> IO.inspect
