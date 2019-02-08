f0dccc12a44761f626a2ffd5ef73d455c99b7375
Richard Bowker <richardbowker@monkeygroover.com>
2019-02-08 23:04:34 +0000

    day2 style

--- day_2/part2-1.exs
+++ day_2/part2-1.exs
@@ -1,31 +1,30 @@
 #! /usr/bin/env elixir
 
-defmodule Day2 do
-    def letter_frequencies(id) do
-        Enum.reduce(
-            String.graphemes(id),
-            %{},
-            fn (char, acc) ->
-                Map.update(acc, char, 1, fn(count) -> count + 1 end)
-            end)
-    end
-
-    def pairs_and_triples_reducer(freq_map, {pairs, triples}) do
-        {pairs + contains_freq(freq_map, 2), triples + contains_freq(freq_map, 3)}
-    end
-
-    defp contains_freq(freq_map, freq) do
-        case Map.values(freq_map) |> Enum.count(&(&1 == freq)) do
-            0 -> 0
-            _ -> 1
-        end
-    end
-
+letter_frequencies = fn(id) ->
+    Enum.reduce(
+        String.graphemes(id),
+        %{},
+        fn (char, acc) ->
+            Map.update(acc, char, 1, fn(count) -> count + 1 end)
+        end)
 end
+
+contains_freq = fn(freq_map, freq) ->
+    case Map.values(freq_map) |> Enum.count(&(&1 == freq)) do
+        0 -> 0
+        _ -> 1
+    end
+end
+
+pairs_and_triples_reducer = fn(freq_map, {pairs, triples}) ->
+    {pairs + contains_freq.(freq_map, 2), triples + contains_freq.(freq_map, 3)}
+end
+
+product = fn {p,t} -> p * t end
 
 IO.stream(:stdio, :line)
 |> Stream.map(&String.trim/1)
-|> Stream.map(&Day2.letter_frequencies/1)
-|> Enum.reduce({0,0}, &Day2.pairs_and_triples_reducer/2)
-|> (fn {p,t} -> p * t end).()
+|> Stream.map(letter_frequencies)
+|> Enum.reduce({0,0}, pairs_and_triples_reducer)
+|> product.()
 |> IO.inspect
