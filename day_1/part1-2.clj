768356a623f58b6ff607ea42cc5e487146eaa6e4
Richard Bowker <richardbowker@monkeygroover.com>
2019-02-08 12:43:49 +0000

    refactor

--- day_1/part1-2.clj
+++ day_1/part1-2.clj
@@ -1,5 +1,8 @@
 #!/usr/bin/env lein exec
 (require '[clojure.string :as str])
 
-(def cycle-values (cycle (map read-string (str/split-lines (slurp *in*)))))
-(print (reduce #(if (contains? %1 %2) (reduced %2) (conj %1 %2)) #{} (reductions + cycle-values)))
+(def freq-seq (reductions + (cycle (map read-string (str/split-lines (slurp *in*))))))
+
+(defn reduce-until-duplicate [freq-stream] (reduce #(if (contains? %1 %2) (reduced %2) (conj %1 %2)) #{} freq-stream))
+
+(print (reduce-until-duplicate freq-seq))
