8cf753560667986703a8f7d769c45339908ba601
Richard Bowker <richardbowker@monkeygroover.com>
2019-12-03 17:52:35 +0000

    part 2

--- 2019/day_3/src/main.rs
+++ 2019/day_3/src/main.rs
@@ -11,98 +11,84 @@ impl Coord {
     fn new(x: i32, y: i32) -> Coord {
         Coord{x: x, y: y}
     }
+
+    fn manhattan(&self) -> i32 {
+        self.x.abs() + self.y.abs()
+    }
+}
+
+enum Direction {
+    Up(i32),
+    Down(i32),
+    Left(i32),
+    Right(i32),
 }
 
 fn main() {
+    let re = Regex::new(r"(R|L|U|D)(\d+)").unwrap();
+
     let directions = include_str!("input.txt")
                                 .trim()
                                 .lines()
                                 .map(|line| {
-                                    line.split(",")
+                                    line.split(",").map(|raw_command| {
+                                        let caps = re.captures(raw_command).unwrap();
+                                        let direction = caps[1].parse::<String>().unwrap().to_owned();
+                                        let dist = caps[2].parse::<i32>().unwrap();
+                                        match direction.as_ref() {
+                                            "U" => Direction::Up(dist),
+                                            "D" => Direction::Down(dist),
+                                            "L" => Direction::Left(dist),
+                                            "R" => Direction::Right(dist),
+                                            _   => panic!("bad direction")
+                                        }
+                                    })
                                 });
 
-    let re = Regex::new(r"(R|L|U|D)(\d+)").unwrap();
+    let coord_vecs: Vec<Vec<Coord>> = directions.map(|wire| {
+        let mut prev_coord = Coord::new(0,0);
+        let mut coord_vec = vec![prev_coord];
 
-    let coord_sets: Vec<HashSet<Coord>> = directions.map(|wire| {
-        println!("--------------");
-        let mut coord_set = HashSet::new();
-        let mut last_coord = Coord::new(0,0);
-        for vector in wire {
-            let caps = re.captures(vector).unwrap();
-            let direction = caps[1].parse::<String>().unwrap().to_owned();
-            let dist = caps[2].parse::<i32>().unwrap();
-
-            let new_coord: Coord = match direction.as_ref() {
-                "U" => {
-                    // println!("U {}", dist);
-                    let new_coord = Coord::new(last_coord.x, last_coord.y+dist);
-
-                    for y in last_coord.y..=new_coord.y {
-                        let fill_coord = Coord::new(last_coord.x, y);
-                        if manhattan(fill_coord) != 0 {
-                            coord_set.insert(fill_coord);
-                            //println!("inserting {:?}", fill_coord);
-                        }
-                    };
-
+        for command in wire {
+            prev_coord = match command {
+                Direction::Up(dist) => {
+                    let new_coord = Coord::new(prev_coord.x, prev_coord.y+dist);
+                    for y in (prev_coord.y+1)..=new_coord.y { coord_vec.push(Coord::new(prev_coord.x, y)); };
                     new_coord
                 },
-                "D" => {
-                    // println!("D {}", dist);
-                    let new_coord = Coord::new(last_coord.x, last_coord.y-dist);
-
-                    for y in new_coord.y..=last_coord.y {
-                        let fill_coord = Coord::new(last_coord.x, y);
-                        if manhattan(fill_coord) != 0 {
-                            coord_set.insert(fill_coord);
-                            // println!("inserting {:?}", fill_coord);
-                        }
-                    };
+                Direction::Down(dist) => {
+                    let new_coord =Coord::new(prev_coord.x, prev_coord.y-dist);
+                    for y in (new_coord.y..prev_coord.y).rev() { coord_vec.push(Coord::new(prev_coord.x, y)); };
                     new_coord
                 },
-                "L" => {
-                    // println!("L {}", dist);
-                    let new_coord = Coord::new(last_coord.x-dist, last_coord.y);
-
-                    for x in new_coord.x..=last_coord.x {
-                        let fill_coord = Coord::new(x, last_coord.y);
-                        if manhattan(fill_coord) != 0 {
-                            coord_set.insert(fill_coord);
-                            // println!("inserting {:?}", fill_coord);
-                        }
-                    };
+                Direction::Left(dist) => {
+                    let new_coord = Coord::new(prev_coord.x-dist, prev_coord.y);
+                    for x in (new_coord.x..prev_coord.x).rev() { coord_vec.push(Coord::new(x, prev_coord.y)); };
                     new_coord
                 },
-                "R" => {
-                    // println!("R {}", dist);
-                    let new_coord = Coord::new(last_coord.x+dist, last_coord.y);
-
-                    for x in last_coord.x..=new_coord.x {
-                        let fill_coord = Coord::new(x, last_coord.y);
-                        if manhattan(fill_coord) != 0 {
-                            coord_set.insert(fill_coord);
-                            // println!("inserting {:?}", fill_coord);
-                        }
-                    };
+                Direction::Right(dist) => {
+                    let new_coord = Coord::new(prev_coord.x+dist, prev_coord.y);
+                    for x in (prev_coord.x+1)..=new_coord.x { coord_vec.push(Coord::new(x, prev_coord.y)); };
                     new_coord
-                },
-                _ => panic!("bad direction")
+                }
             };
-
-            // println!("new coord: {:?}", new_coord);
-
-            last_coord = new_coord;
         }
-        coord_set
+        coord_vec
     }).collect();
 
+    let wire_1_point_set: HashSet<Coord> = coord_vecs[0].iter().cloned().collect();
+    let wire_2_point_set: HashSet<Coord> = coord_vecs[1].iter().cloned().collect();
+    let intersections = wire_1_point_set.intersection(&wire_2_point_set).filter(|coord| **coord != Coord::new(0,0));
+    let min = intersections.clone().min_by(|a , b| a.manhattan().cmp(&b.manhattan())).unwrap();
 
-    let intersections = coord_sets[0].intersection(&coord_sets[1]);
-    let max = intersections.min_by(|a , b| manhattan(**a).cmp(&manhattan(**b))).unwrap();
+    println!("part 1: {:?}", min.manhattan() );
 
-    println!("{:?}", manhattan(*max));
-}
+    // walk each wire looking for the intersection points
+    let minimum_path = intersections.map( |intersection| {
+        let path_1_len = coord_vecs[0].iter().cloned().position(|coord| coord == *intersection).unwrap();
+        let path_2_len = coord_vecs[1].iter().cloned().position(|coord| coord == *intersection).unwrap();
+        path_1_len + path_2_len
+    } ).min().unwrap();
 
-fn manhattan(coord: Coord) -> i32 {
-    coord.x.abs() + coord.y.abs()
+    println!("part 2: {:?}", minimum_path);
 }
