c21547109b02607a77c7525f8a744ac86bf843f1
Richard Bowker <richardbowker@monkeygroover.com>
2019-12-05 10:29:52 +0000

    day 5 - part 1

---
+++ 2019/day_5/src/main.rs
@@ -0,0 +1,106 @@
+static INPUT: i32 = 1;
+
+fn main() {
+    let initial_memory: Vec<i32> = include_str!("input.txt")
+                                .trim()
+                                .split(",")
+                                .map(|s| s.parse::<i32>().unwrap())
+                                .collect();
+
+    run(&mut initial_memory.clone());
+}
+
+fn run(memory: &mut Vec<i32>) -> i32 {
+    let mut pc :usize = 0;
+    while let Some(ops) = handle_op(pc, memory) {
+        pc = pc + ops;
+    }
+
+    memory[0]
+}
+
+fn handle_op(pc: usize, memory: &mut Vec<i32>) -> Option<usize> {
+    // split op code
+    let op = memory[pc] % 100;
+    let mode_list = vec![(memory[pc]/100) % 10, (memory[pc]/1000) % 10, (memory[pc]/10000) % 10];
+
+    //println!("op {:?}, modes: {:?}", op, mode_list);
+
+    match op {
+        99 => None,
+        1 => add(pc, memory, mode_list),
+        2 => multiply(pc, memory, mode_list),
+        3 => store(pc, memory, mode_list),
+        4 => output(pc, memory, mode_list),
+        _ => panic!("bad op code")
+    }
+}
+
+fn add(pc: usize, memory: &mut Vec<i32>, modes: Vec<i32>) -> Option<usize> {
+    //println!("memory {:?}, pc {}", memory, pc);
+    let param1 = memory[pc+1 as usize];
+    //println!("param1 {}", param1);
+    let a = match modes[0] {
+        0 => memory[param1 as usize],
+        1 => param1,
+        _ => panic!("bad mode")
+    };
+
+    let param2 = memory[pc+2 as usize];
+    //println!("param2 {}", param2);
+    let b = match modes[1] {
+        0 => memory[param2 as usize],
+        1 => param2,
+        _ => panic!("bad mode")
+    };
+
+    let dest = memory[pc+3];
+
+    memory[dest as usize] = a + b;
+    Some(4)
+}
+
+fn multiply(pc: usize, memory: &mut Vec<i32>, modes: Vec<i32>) -> Option<usize> {
+    //println!("memory {:?}, pc {}", memory, pc);
+    let param1 = memory[pc+1 as usize];
+    //println!("param1 {}", param1);
+    let a = match modes[0] {
+        0 => memory[param1 as usize],
+        1 => param1,
+        _ => panic!("bad mode")
+    };
+
+    let param2 = memory[pc+2 as usize];
+    //println!("param2 {}", param2);
+    let b = match modes[1] {
+        0 => memory[param2 as usize],
+        1 => param2,
+        _ => panic!("bad mode")
+    };
+
+    let dest = memory[pc+3];
+
+   // println!("multiplying {} with {} to {}", a, b, dest);
+
+    memory[dest as usize] = a * b;
+    Some(4)
+}
+
+fn store(pc: usize, memory: &mut Vec<i32>, _modes: Vec<i32>) -> Option<usize> {
+    let dest = memory[pc+3];
+
+    //println!("storing {} to {}", INPUT, dest);
+    memory[dest as usize] = INPUT;
+    Some(2)
+}
+
+fn output(pc: usize, memory: &mut Vec<i32>, modes: Vec<i32>) -> Option<usize> {
+    let out = match modes[0] {
+        0 => memory[memory[pc+1 as usize] as usize],
+        1 => memory[pc+1 as usize],
+        _ => panic!("bad mode")
+    };
+
+    println!("output {}", out);
+    Some(2)
+}
