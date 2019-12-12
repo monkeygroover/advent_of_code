911d3271e8725440a1c31a9761e385e1192feebc
Richard Bowker <richardbowker@monkeygroover.com>
2019-12-12 17:01:47 +0000

    part 2

--- 2019/day_12/src/main.rs
+++ 2019/day_12/src/main.rs
@@ -1,6 +1,8 @@
 use log::*;
 use regex::Regex;
 
+use num_integer::*;
+
 #[derive(Eq, PartialEq, Debug, Copy, Clone, Hash)]
 struct Coord {
     x: i32,
@@ -62,6 +64,18 @@ impl Moon {
         let kin = self.vel.x.abs() +self.vel.y.abs() + self.vel.z.abs();
         pot * kin
     }
+
+    fn cmp_x(self, other: Moon) -> bool {
+        self.pos.x == other.pos.x && self.vel.x == other.vel.x
+    }
+
+    fn cmp_y(self, other: Moon) -> bool {
+        self.pos.y == other.pos.y && self.vel.y == other.vel.y
+    }
+
+    fn cmp_z(self, other: Moon) -> bool {
+        self.pos.z == other.pos.z && self.vel.z == other.vel.z
+    }
 }
 
 fn main() {
@@ -69,7 +83,7 @@ fn main() {
 
     let re = Regex::new(r"<x=(-?\d+), y=(-?\d+), z=(-?\d+)>").unwrap();
 
-    let mut moons: Vec<Moon> = include_str!("input.txt")
+    let moons: Vec<Moon> = include_str!("input.txt")
     .trim()
     .lines()
     .map(|s| {
@@ -81,31 +95,75 @@ fn main() {
     })
     .collect();
 
-    //let imm_moons = moons.clone();
+    let mut moons1 = moons.clone();
 
     for step in 0..1000 {
         trace!("step {}", step);
-        for moon in moons.clone() {
+        for moon in moons1.clone() {
             trace!("{:?}", moon);
         }
 
-        let temp_moons = moons.clone();
+        let temp_moons = moons1.clone();
 
-        for moon in moons.iter_mut() {
+        for moon in moons1.iter_mut() {
             moon.update_velocity(&temp_moons)
         }
 
-        for moon in moons.iter_mut() {
+        for moon in moons1.iter_mut() {
             moon.update_position()
         }
     }
 
     trace!("FINAL");
-    for moon in moons.clone() {
+    for moon in moons1.clone() {
         trace!("{:?}", moon);
     }
 
-    let total_energy =  moons.iter().fold(0, |acc, moon| acc + moon.energy());
+    let total_energy = moons1.iter().fold(0, |acc, moon| acc + moon.energy());
 
     println!("part1 {}", total_energy);
+
+    let mut cycle_x: Option<i64> = None;
+    let mut cycle_y: Option<i64> = None;
+    let mut cycle_z: Option<i64> = None;
+    let mut step = 0;
+
+    let mut moons2 = moons.clone();
+
+    loop {
+        step += 1;
+        let temp_moons = moons2.clone();
+
+        for moon in moons2.iter_mut() {
+            moon.update_velocity(&temp_moons)
+        }
+
+        for moon in moons2.iter_mut() {
+            moon.update_position()
+        }
+
+        if cycle_x == None {
+            if moons.iter().zip(moons2.iter()).all(|(a, b)| a.cmp_x(*b)) {
+                cycle_x = Some(step);
+            }
+        }
+
+        if cycle_y == None {
+            if moons.iter().zip(moons2.iter()).all(|(a, b)| a.cmp_y(*b)) {
+                cycle_y = Some(step);
+            }
+        }
+
+        if cycle_z == None {
+            if moons.iter().zip(moons2.iter()).all(|(a, b)| a.cmp_z(*b)) {
+                cycle_z = Some(step);
+            }
+        }
+
+        if cycle_x != None && cycle_y != None && cycle_z != None { break }
+    }
+
+    println!("part2 {:?} {:?} {:?}", cycle_x, cycle_y, cycle_z);
+    let lcm = cycle_x.unwrap().lcm(&cycle_y.unwrap()).lcm(&cycle_z.unwrap());
+    println!("part2 {}", lcm);
 }
