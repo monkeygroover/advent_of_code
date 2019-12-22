10825f50b907b17438496c56ded93006b7f1eeba
Richard Bowker <richardbowker@monkeygroover.com>
2019-12-22 20:29:43 +0000

    day 21 - part 1

---
+++ 2019/day_21/src/main.rs
@@ -0,0 +1,55 @@
+use log::*;
+
+mod vm;
+use crate::vm::{VM, State};
+
+fn main() {
+    env_logger::init();
+
+    let mut initial_memory: Vec<i64> = include_str!("input.txt")
+    .trim()
+    .split(",")
+    .map(|s| s.parse::<i64>().unwrap())
+    .collect();
+
+    initial_memory.resize(1048576, 0);
+
+    let script1 =
+"NOT A J
+NOT C T
+AND D T
+OR T J
+WALK
+";
+
+    println!("part1 damage {}", run_springscript(script1, initial_memory));
+}
+
+fn run_springscript(script: &str, initial_memory: Vec<i64>) -> i64 {
+    let mut bot = VM::new("Robot", initial_memory);
+
+    let mut script_iter = script.chars();
+    loop {
+        match bot.run() {
+            State::InputPending => {
+                match script_iter.next() {
+                    Some(value) => {
+                        let next_input = value as i64;
+                        info!("=> {}", next_input);
+                        bot.handle_input(next_input);
+                    },
+                    None => panic!()
+                }
+            },
+            State::OutputProduced(val) => {
+                if val < 256 {
+                    print!("{}", val as u8 as char);
+                } else {
+                    return val;
+                }
+            },
+            State::Halt => {info!("Halt")},
+            State::Continue => ()
+        }
+    }
+}
