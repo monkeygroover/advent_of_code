7b5c996ca578ee878aa1cb85a579d2d855500340
Richard Bowker <richardbowker@monkeygroover.com>
2020-12-01 10:08:58 +0000

    find/map version

--- 2020/day_1/src/main.rs
+++ 2020/day_1/src/main.rs
@@ -1,20 +1,15 @@
 use itertools::Itertools;
 
 fn main() {
-    let input_iter = include_str!("input.txt")
-                                 .trim()
-                                 .lines()
-                                 .map(|s| s.parse::<u32>().unwrap());
+    let input = include_str!("input.txt").trim().lines().map(|s| s.parse::<u32>().unwrap());
 
-    for v in input_iter.clone().combinations(2) {
-        if v[0] + v[1] == 2020 {
-            println!("{}", v[0] * v[1])
-        }
-    }
+    let part1 = input.clone().combinations(2)
+                             .find(|v| v[0] + v[1] == 2020)
+                              .map(|v| v[0] * v[1]);
 
-    for v in input_iter.combinations(3) {
-        if v[0] + v[1] + v[2] == 2020 {
-            println!("{}", v[0] * v[1] * v[2])
-        }
-    }
+    let part2 = input.combinations(3)
+                     .find(|v| v[0] + v[1] + v[2] == 2020)
+                      .map(|v| v[0] * v[1] * v[2]);
+
+    println!("{}, {}", part1.unwrap(), part2.unwrap());
 }
