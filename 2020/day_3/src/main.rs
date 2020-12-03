4babaca960fdd4ef5a3400c771a6a26b18fbfb29
Richard Bowker <richardbowker@monkeygroover.com>
2020-12-03 09:48:10 +0000

    tweaks

--- 2020/day_3/src/main.rs
+++ 2020/day_3/src/main.rs
@@ -15,25 +15,18 @@ impl Coord {
 fn main() {
     env_logger::init();
 
-    let height = include_str!("input.txt")
+    let lines = include_str!("input.txt")
     .trim()
-    .lines()
-    .count() as u32;
+    .lines();
 
-    let width = include_str!("input.txt")
-    .trim()
-    .lines()
-    .nth(1).unwrap()
-    .chars()
-    .count() as u32;
+    let height = lines.clone().count() as u32;
+    let width = lines.clone().nth(1).unwrap().chars().count() as u32;
+    let grid_size = Coord::new(width, height);
 
     trace!("{}, {}", height, width);
 
     let trees: Vec<Coord> =
-        include_str!("input.txt")
-        .trim()
-        .lines()
-        .enumerate()
+        lines.enumerate()
         .flat_map(|(y, line)| {
             line.chars().enumerate().flat_map(move |(x, c)| {
                 match c {
@@ -45,23 +38,28 @@ fn main() {
             })
         .collect();
 
-    let tree_count1 = count_trees_on_path(1,1,trees.clone(), width, height);
-    let tree_count2 = count_trees_on_path(3,1,trees.clone(), width, height);
-    let tree_count3 = count_trees_on_path(5,1,trees.clone(), width, height);
-    let tree_count4 = count_trees_on_path(7,1,trees.clone(), width, height);
-    let tree_count5 = count_trees_on_path(1,2,trees, width, height);
+    let path_vectors = vec![Coord::new(1,1),
+                            Coord::new(3,1),
+                            Coord::new(5,1),
+                            Coord::new(7,1),
+                            Coord::new(1,2)];
 
-    println!("{}, {}", tree_count2, tree_count1 * tree_count2 * tree_count3 * tree_count4 * tree_count5);
+    let counts = path_vectors.iter().map(|delta| count_trees_on_path(*delta, trees.clone(), grid_size));
+
+    let part1 = counts.clone().nth(1).unwrap();
+    let part2: u32 = counts.product();
+
+    println!("{}, {}", part1, part2);
 }
 
-fn count_trees_on_path(x_step: u32, y_step: u32, trees: Vec<Coord>, repeat: u32, height: u32) -> u32 {
+fn count_trees_on_path(delta: Coord, trees: Vec<Coord>, grid_size: Coord) -> u32 {
     let mut tob_coord = Coord::new(0,0);
     let mut tree_count = 0;
 
-    while tob_coord.y < height {
-        tob_coord.y += y_step;
-        tob_coord.x += x_step;
-        tob_coord.x = tob_coord.x % repeat;
+    while tob_coord.y < grid_size.y {
+        tob_coord.y += delta.y;
+        tob_coord.x += delta.x;
+        tob_coord.x = tob_coord.x % grid_size.x;
 
         if trees.iter().any(|tree| *tree == tob_coord) {
             tree_count+=1;
