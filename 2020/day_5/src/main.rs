2c3607453c4243e92ed0a47a9da47b2033b07756
Richard Bowker <richardbowker@monkeygroover.com>
2020-12-05 17:48:34 +0000

    moooooooor cleanup

--- 2020/day_5/src/main.rs
+++ 2020/day_5/src/main.rs
@@ -1,18 +1,13 @@
 use itertools::Itertools;
 
 fn main() {
-    let input = include_str!("input.txt").trim().lines().map(|s| s.to_string());
-
-    let seat_ids = input.map(|line| {
-        let binary_str = line.replace("F", "0").replace("B", "1").replace("L", "0").replace("R", "1");
-        let row = isize::from_str_radix(&binary_str[..7], 2).unwrap();
-        let column = isize::from_str_radix(&binary_str[7..10], 2).unwrap();
-        row * 8 + column
-    });
+    let seat_ids = include_str!("input.txt").trim().lines()
+        .map(|s| {
+            let binary_str = s.to_string().replace("F", "0").replace("B", "1").replace("L", "0").replace("R", "1");
+            isize::from_str_radix(&binary_str, 2).unwrap() });
 
     let part1 = seat_ids.clone().max().unwrap();
-    let (seat_before_gap, _) = seat_ids.sorted().tuple_windows().find(|(a, b)| *b != a + 1).unwrap();
-    let part2 = seat_before_gap + 1;
+    let part2 = seat_ids.sorted().tuple_windows().find(|(a, b)| *b != a + 1).map(|(seat_before_gap, _)| seat_before_gap + 1 ).unwrap();
     
     println!("{} {}", part1, part2);
 }
