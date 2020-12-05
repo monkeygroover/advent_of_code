71f1801d0d11eafa10ecd33f6c7ebcdfbbb74ff1
Richard Bowker <richardbowker@monkeygroover.com>
2020-12-05 13:30:04 +0000

    cleanups

--- 2020/day_5/src/main.rs
+++ 2020/day_5/src/main.rs
@@ -11,28 +11,27 @@ fn main() {
     let seat_ids = input.map(|line| {
         let first_7 = line.chars().take(7);
         let last_3 = line.chars().skip(7).take(3);
-        first_7.fold((row_list.clone(), last_3),
-            |(mut acc, last_3), half| match half {
+
+        let column = first_7.fold(row_list.clone(),
+            |mut acc, half| match half {
                 'F' => {
                     let split_point = acc.len() / 2;
                     acc.truncate(split_point);
-                    (acc, last_3)
+                    acc
                 },
                 'B' => {
                     let split_point = acc.len() / 2;
                     let split = acc.split_off(split_point);
-                    (split, last_3)
+                    split
                 },
                 _ => panic!("Bad value")
             }
-        )
-        }
-        )
-        .map(|(i, last_3)| {
-            let row = last_3.fold(column_list.clone(),
+        );
+
+        let row = last_3.fold(column_list.clone(),
             |mut acc, half| match half {
                 'L' => {
-                    let split_point = acc.len() / 2;
+                let split_point = acc.len() / 2;
                     acc.truncate(split_point);
                     acc
                 },
@@ -43,10 +42,10 @@ fn main() {
                 },
                 _ => panic!("Bad value")
             }
-            );
-            (i[0], row[0])
-        })
-        .map(|(a, b)| a * 8 + b);
+        );
+
+        column[0] * 8 + row[0]
+    });
 
     let part1 = seat_ids.clone().max().unwrap();
 
