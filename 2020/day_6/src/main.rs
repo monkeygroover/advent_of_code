b6b393891db0d609564847a71e51f4b3fdfcf323
Richard Bowker <richardbowker@monkeygroover.com>
2020-12-06 10:45:51 +0000

    day 6 - part 1

--- 2020/day_6/src/main.rs
+++ 2020/day_6/src/main.rs
@@ -1,4 +1,22 @@
+use std::collections::HashSet;
 fn main() {
-    let input = include_str!("input").trim().lines();
-    println!("{:?}", input);
+    let cleaned_input = include_str!("input.txt")
+    .trim()
+    .replace("\n\n", ";")
+    .replace("\n", "");
+
+    let data: Vec<HashSet<char>> = cleaned_input
+                .split(';')
+                .map(|line| {
+                    let mut chars = HashSet::new();
+                    for ch in line.chars() {
+                        chars.insert(ch);
+                    }
+                    chars
+                })
+                .collect();
+
+    let part1 = data.iter().fold(0, |acc, set| acc + set.len());
+
+    println!("{:?}", part1);
 }
