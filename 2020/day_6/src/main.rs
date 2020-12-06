6286c535a1bb3cab3367bb32f5eb04e4c9e04daf
Richard Bowker <richardbowker@monkeygroover.com>
2020-12-06 11:04:06 +0000

    part 2

--- 2020/day_6/src/main.rs
+++ 2020/day_6/src/main.rs
@@ -2,10 +2,10 @@ use std::collections::HashSet;
 fn main() {
     let cleaned_input = include_str!("input.txt")
     .trim()
-    .replace("\n\n", ";")
-    .replace("\n", "");
+    .replace("\n\n", ";");
 
     let part1 = cleaned_input
+                .replace("\n", "")
                 .split(';')
                 .fold(0, |acc, line| {
                     let mut chars = HashSet::new();
@@ -15,5 +15,21 @@ fn main() {
                     acc + chars.len()
                 });
 
-    println!("{:?}", part1);
+    let part2 = cleaned_input
+    .split(';')
+    .fold(0, |acc, group| {
+        let mut answers: Vec<HashSet<char>> = vec![];
+        for person in group.lines() {
+            let mut chars = HashSet::new();
+            for ch in person.chars() {
+                chars.insert(ch);
+            }
+            answers.push(chars)
+        }
+        let intersection_set = answers.iter().fold(answers[0].clone(), |acc, a| acc.intersection(&a).cloned().collect());
+        acc + intersection_set.len()
+    });
+
+
+    println!("{} {}", part1, part2);
 }
