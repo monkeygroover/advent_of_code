f405e7260b327ca5c54c5e586309856608e75f0e
Richard Bowker <richardbowker@monkeygroover.com>
2020-12-10 10:06:00 +0000

    day 10 - part 1

--- 2020/day_10/src/main.rs
+++ 2020/day_10/src/main.rs
@@ -1,9 +1,30 @@
 fn main() {
-    let input: Vec<i32> = include_str!("input.txt")
+    let mut input: Vec<i32> = include_str!("input.txt")
     .trim()
     .lines()
     .map(|s| { s.parse::<i32>().unwrap()} )
     .collect();
 
-    println!("{:?}", input)
+    input.sort();
+
+    input.insert(0,0);
+    input.push(input.iter().max().map(|x| x + 3).unwrap());
+    let input = input;
+
+    println!("{:?}", input);
+
+    let (diffs_of_one, diffs_of_three) = 
+        input.windows(2)
+        .fold( (0,0), |(o, t), j_pair| {
+            let diff = j_pair[1] -j_pair[0];
+            match diff {
+                1 => (o+1, t),
+                3 => (o, t+1),
+                _ => unreachable!()
+            }
+        });
+
+    let part1 = diffs_of_one * diffs_of_three;
+
+    println!("{}*{}={}", diffs_of_one, diffs_of_three, part1)
 }
