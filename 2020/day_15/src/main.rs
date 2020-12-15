172c9b79416d589010baac0a578d3efc1e9d88d6
Richard Bowker <richardbowker@monkeygroover.com>
2020-12-15 17:25:36 +0000

    cleanup

--- 2020/day_15/src/main.rs
+++ 2020/day_15/src/main.rs
@@ -10,17 +10,18 @@ fn main() {
     let mut input_iter = input.iter();
 
     // remember the last two indices of each number spoken 
-    let mut historical_spoken_data: HashMap<i64, (Option<i64>, Option<i64>)> = HashMap::new(); // when was this last spoken?
-    let mut index: i64 = 0;
-    let mut last_number_spoken: i64 = 0; // last number spoken
+    let mut historical_spoken_data: HashMap<i64, (Option<i64>, Option<i64>)> = HashMap::new(); // turn_numbers of last two times this number was spoken
+    
+    let mut turn_number: i64 = 0;
+    let mut last_number_spoken: i64 = 0; 
 
     let elves_iter = std::iter::from_fn(move || {
-        index += 1;
+        turn_number += 1;
         debug!("");
-        debug!("index is {} last spoken was {}", index, last_number_spoken);
+        debug!("turn_number is {} last spoken was {}", turn_number, last_number_spoken);
         if let Some(&input_val) = input_iter.next() {
             // still reading the initial list, these are all the first time spoken
-            historical_spoken_data.insert(input_val, (Some(index), None));
+            historical_spoken_data.insert(input_val, (Some(turn_number), None));
             last_number_spoken = input_val;
             debug!("reading input list, speaking {}", input_val);
             Some(input_val)
@@ -30,33 +31,25 @@ fn main() {
             let previous_indices = historical_spoken_data.get(&last_number_spoken).unwrap();
             
             match previous_indices {
-                (Some(_), None) => {
+                (Some(_), None) => { // only spoken once before
                     debug!("{} last time was the first time, speaking 0", last_number_spoken);
                     last_number_spoken = 0;
-                    debug!("speaking 0 a");
-                    //get the index of the last time zero was spoken
-                    let (last_zero, _) = historical_spoken_data[&0];
-                    historical_spoken_data.insert(0, (Some(index), last_zero));
+                    debug!("speaking 0");
+                    //get the turn_number of the last time zero was spoken
+                    let previous_zero: Option<i64> = historical_spoken_data.get(&0).map(|&(x, _)| x).flatten();
+                    historical_spoken_data.insert(0, (Some(turn_number), previous_zero));
                     Some(0)
                 }
-                (Some(last_index), Some(second_last_index)) => {
-                    let delta = last_index - second_last_index;
-                    debug!("{} was spoken before, {} - {} -> delta {}", last_number_spoken, last_index, second_last_index, delta);
+                (Some(turn_last_spoken), Some(turn_second_last_spoken)) => {
+                    let turn_delta = turn_last_spoken - turn_second_last_spoken;
+                    debug!("{} was spoken before, {} - {} -> delta {}", last_number_spoken, turn_last_spoken, turn_second_last_spoken, turn_delta);
 
-                    //get the index of the last time delta was spoken
-                    if historical_spoken_data.contains_key(&delta) {
-                        let (last_delta, _) = historical_spoken_data[&delta];
-
-                        historical_spoken_data.insert(delta, (Some(index), last_delta));
-                        last_number_spoken = delta;
-                        debug!("speaking {}", delta);
-                        Some(delta)
-                    } else {
-                        historical_spoken_data.insert(delta, (Some(index), None));
-                        last_number_spoken = delta;
-                        debug!("speaking {}", delta);
-                        Some(delta)
-                    }
+                    //get the turn_number (if exists) of the last time delta was spoken
+                    let previous_delta: Option<i64> = historical_spoken_data.get(&turn_delta).map(|&(x, _)| x).flatten();
+                    historical_spoken_data.insert(turn_delta, (Some(turn_number), previous_delta));
+                    last_number_spoken = turn_delta;
+                    debug!("speaking {}", turn_delta);
+                    Some(turn_delta)
                 },
                 (None, _) => unreachable!()
             }
