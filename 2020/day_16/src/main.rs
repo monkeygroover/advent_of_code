14d1eb34028a0ee0691c88408d2a9f08c92b6e8a
Richard Bowker <richardbowker@monkeygroover.com>
2020-12-16 23:25:49 +0000

    oh my

--- 2020/day_16/src/main.rs
+++ 2020/day_16/src/main.rs
@@ -62,28 +62,35 @@ fn main() {
         }
     }
    
-    // for col in valid_rules_for_column.iter().enumerate() { println!("{:?}", col); }
-    let mut found_so_far: Vec<usize> = vec![];
-
     for (col, rules) in valid_rules_for_column.iter().enumerate().sorted_by(|&(_, a), &(_, b)| Ord::cmp(&a.len(), &b.len()) ) { 
         println!("{:?} -> {:?}", col, rules); 
-        // let newly_found = rules - *found_so_far;
-
     }
 
-    // solve this for unique columns, noting that there is a column with 1, 2, 3...
-    // let mut rules_for_column: [usize; 20] = Default::default();
+    //   this prints
 
-    // for _ in 0..20 {
-    //     // find the shortest, take that number, insert it into answer list
-    //     // remove that number from all, repeat..
-    //     if let Some((col, rule)) = valid_rules_for_column.iter().enumerate().min_by(|&(_, a), &(_, b)| Ord::cmp(&a.len(), &b.len()) ) {
-    //         println!("found {:?} in col {}", rule[0], col);
-    //         rules_for_column[col] = rule[0];
+    // 12 -> [9]
+    // 6 -> [9, 17]
+    // 0 -> [9, 16, 17]
+    // 5 -> [8, 9, 16, 17]
+    // 16 -> [7, 8, 9, 16, 17]
+    // 2 -> [7, 8, 9, 16, 17, 18]
+    // 15 -> [5, 7, 8, 9, 16, 17, 18]
+    // 13 -> [4, 5, 7, 8, 9, 16, 17, 18]
+    // 17 -> [1, 4, 5, 7, 8, 9, 16, 17, 18]
+    // 3 -> [0, 1, 4, 5, 7, 8, 9, 16, 17, 18]
+    // 9 -> [0, 1, 2, 4, 5, 7, 8, 9, 16, 17, 18]
+    // 1 -> [0, 1, 2, 3, 4, 5, 7, 8, 9, 16, 17, 18]
+    // 10 -> [0, 1, 2, 3, 4, 5, 7, 8, 9, 16, 17, 18, 19]
+    // 7 -> [0, 1, 2, 3, 4, 5, 7, 8, 9, 13, 16, 17, 18, 19]
+    // 19 -> [0, 1, 2, 3, 4, 5, 7, 8, 9, 13, 14, 16, 17, 18, 19]
+    // 4 -> [0, 1, 2, 3, 4, 5, 7, 8, 9, 11, 13, 14, 16, 17, 18, 19]
+    // 18 -> [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 13, 14, 16, 17, 18, 19]
+    // 8 -> [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 16, 17, 18, 19]
+    // 11 -> [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+    // 14 -> [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
 
-    //     }
+   // lost the will to live at this point and did the rest by hand looking for the value added
+   // in each step, then finding the values in "your ticket" for those columns
 
-    // }
-
-
+   println!("{}", 151u64 * 101 * 157 *  139 * 97 * 149);
 }
